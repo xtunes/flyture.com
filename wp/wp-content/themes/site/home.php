bb096a92a742ff76b547e4427b6e730ac2cf6fdb
Eric <eric@xtunes.cc>
2011-10-08 13:44:39 +0800

    添加首页flash

--- wp/wp-content/themes/site/home.php
+++ wp/wp-content/themes/site/home.php
@@ -8,9 +8,41 @@ get_header(); ?>
 
     <div id="main" role="main">
     	<div class="container">
-		<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-					<?php the_content(); ?>
-		<?php endwhile; // end of the loop. ?>
+    	<div class="homeswf">
+		<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="910" height="460" id="shape 26" align="middle">
+				<param name="movie" value="/swf/SlideShow.swf" />
+				<param name="quality" value="high" />
+				<param name="bgcolor" value="#cccccc" />
+				<param name="play" value="true" />
+				<param name="loop" value="true" />
+				<param name="wmode" value="transparent" />
+				<param name="scale" value="showall" />
+				<param name="menu" value="true" />
+				<param name="devicefont" value="false" />
+				<param name="salign" value="" />
+				<param name="allowScriptAccess" value="sameDomain" />
+				<!--[if !IE]>-->
+				<object type="application/x-shockwave-flash" data="/swf/SlideShow.swf" width="910" height="460">
+					<param name="movie" value="/swf/SlideShow.swf" />
+					<param name="quality" value="high" />
+					<param name="bgcolor" value="#cccccc" />
+					<param name="play" value="true" />
+					<param name="loop" value="true" />
+					<param name="wmode" value="transparent" />
+					<param name="scale" value="showall" />
+					<param name="menu" value="true" />
+					<param name="devicefont" value="false" />
+					<param name="salign" value="" />
+					<param name="allowScriptAccess" value="sameDomain" />
+				<!--<![endif]-->
+					<a href="http://www.adobe.com/go/getflash">
+						<img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="获得 Adobe Flash Player" />
+					</a>
+				<!--[if !IE]>-->
+				</object>
+				<!--<![endif]-->
+			</object>
+		</div>
 		</div>
     </div>
  <!--! end of #container -->
